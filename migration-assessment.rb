33715467294a59d5878adf1ed5c41e87fafb0bb6
v-cstrakaa <cameron.straka@pnmac.com>
2019-07-24 14:56:05 -0400

    Gets matrix of chef client version/OS from API
    Signed-off-by: v-cstrakaa <cameron.straka@pnmac.com>

--- migration-assessment.rb
+++ migration-assessment.rb
@@ -1,52 +1,36 @@
-#!/c/opscode/chefdk/embedded/bin/ruby.exe
-
-# require 'json'
-
-# json_file = File.read('dump.json')
-
-# nodes_list = JSON.parse(json_file)['rows']
-
-# version_list = []
-
-# nodes_list.each do |node|
-#   node.each do |_fqdn, attributes|
-#     version_list << attributes['chef_packages.chef.version']
-#   end
-# end
-
-# version_count = {}
-
-# version_list.each do |version_number|
-#   count = {}
-#   nodes_list.each do |node|
-#     node.each do |_fqdn, attributes|
-#       if attributes['chef_packages.chef.version'] == version_number
-#         count[attributes['platform_family']] = count[attributes['platform_family']] += 1 if count[attributes['platform_family']]
-#         count[attributes['platform_family']] = 1 unless count[attributes['platform_family']]
-#       end
-#     end
-#   end
-#   version_count[version_number] = count
-# end
-# puts "\n\n\n#{version_count}"
-
 #!/c/opscode/chefdk/embedded/bin/ruby
 
 require 'chef'
 
 Chef::Config.from_file("c:/Users/conobcu/.chef/knife.rb")
 rest = Chef::ServerAPI.new(Chef::Config[:chef_server_url])
-# 'name' => ['name']
-nodes = Chef::Search::Query.new(Chef::Config[:chef_server_url]).search(:node, '*:*', rows: 5000, :filter_result => { 'version' => ['chef_packages','chef', 'version']} ).first
+nodes = Chef::Search::Query.new(Chef::Config[:chef_server_url]).search(:node, '*:*', rows: 5000, :filter_result => { 'version' => ['chef_packages','chef', 'version'], 'os' => ['platform'], 'os_version' =>['platform_version']} ).first
 
-nodes_arr = nodes.flat_map{ |ver| ver.values }
-counts = Hash.new(0)
+version_list = []
+nodes.each do  |k|
+  version_list << k['version']
+end
+version_list = version_list.uniq.sort_by{ |i| [i ? 1:0, i]}
 
-nodes_arr.each { |node| counts[node] +=1 }
-total_count = 0
-counts.each do | _ver, count|
-  total_count += count
+nodes_arr = {}
+version_list.each do |version|
+  count = {}
+  nodes.each do |node_data|
+    fq_os = "#{node_data['os']}_#{node_data['os_version']}" unless node_data['os'].nil?
+    fq_os = 'nil' if node_data['os'].nil?
+    if node_data['version'] == version
+      if count[fq_os]
+        count[fq_os] += 1
+      else
+        count[fq_os] = 1
+      end
+    end
+  end
+  nodes_arr[version] = count
 end
 
-puts counts
-puts "\n\n\n Total count: #{total_count}"
+puts "\nVersion: Count"
+nodes_arr.each do | k, v|
+  puts "#{k}: #{v}" unless k.nil?
+  puts "nil: #{v}" if k.nil?
+end
